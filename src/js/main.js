929d6a92474b31c22fdb3800aa4d84d0ab7d38d6
dovgenik <dovgenik@gmail.com>
2025-03-22 20:08:14 +0200

    hw 11 Observer Intersection v2. Add elem. to bottom

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,18 @@
-import axios from 'axios';
-import { createGalleryItem, deleteGalleryItem} from './render-functions.js';
-export { observerIntersection };
-let textSearch = 'rhododendron';
-let pageNum = 1;
-let pageLen = 40;
+import {mainVar, createGalleryItem, deleteGalleryItem, intersectionSet, lightboxRefresh,axiosCall,  page, } from './render-functions.js';
 
-function axiosCall(text, pageN, pageL) {
-  axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: '49309273-01bbbdbc5dd72a8afdb67bc06',
-        q: text,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: pageN,
-        per_page: pageL,
-      },
-    })
-    .then(response => {
-      createGalleryItem(response.data.hits);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
 
-const callbackIntersection = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log(`Елемент "${entry.target.classList[1]}" тут`);
-    } else {
-      console.log(`Елемент "${entry.target.classList[1]}" пішов геть`);
-    }
-  });
-};
-//**********************************
 
-const observerIntersection = new IntersectionObserver(callbackIntersection);
+//*********   Start  ********************************************
+
+intersectionSet();
+
 
 document.querySelector('.form').addEventListener('submit', function (event) {
   event.preventDefault();
   deleteGalleryItem();
-  axiosCall(event.target.elements['search-text'].value, 5, 18);
+  mainVar.searchText=event.target.elements['search-text'].value;
+  axiosCall(mainVar.searchText, page, 18);
 });
 
 //***************************************************** 
 
-
-
-//axiosCall(textSearch, pageNum, pageLen);
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,24 +1,48 @@
+import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { observerIntersection } from './main.js';
 
-export { createGalleryItem, deleteGalleryItem};
-let lightbox;
 
-//**********************************  IntersectionObserver  // поява обєкта в полі видимості*************************************************
-const callbackIntersection = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log(`Елемент "${entry.target.classList[1]}" тут`);
-    } else {
-      console.log(`Елемент "${entry.target.classList[1]}" пішов геть`);
-    }
-  });
+export let page = 1;
+export const mainVar ={
+  run: 1,
+  searchText: '',
+  runStarted: false,
+};
+export {
+  createGalleryItem,
+  deleteGalleryItem,
+  intersectionSet,
+  lightboxRefresh,
+  axiosCall,
 };
 
+let lightbox;
 
+function axiosCall(text, pageN, pageL) {
+  axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: '49309273-01bbbdbc5dd72a8afdb67bc06',
+        q: text,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: pageN,
+        per_page: pageL,
+      },
+    })
+    .then(response => {
+      createGalleryItem(response.data.hits);
+
+      lightboxRefresh();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
 function deleteGalleryItem() {
   document.querySelector('.gallery').outerHTML = `<ul class="gallery"></ul>`;
@@ -36,11 +60,11 @@ function createGalleryItem(arrayImgs) {
   } else {
     const fragment = document.createDocumentFragment();
     const parentUl = document.querySelector('.gallery');
-    
+
     for (const element of arrayImgs) {
       const li = document.createElement('li');
       li.classList.add('gallery-item');
-      
+
       li.innerHTML = `<div class="gallery-div">
         <a class="gallery-link" href="${element.largeImageURL}">
           <img class="gallery-image" src="${element.webformatURL}" alt="${element.tags}">
@@ -57,28 +81,60 @@ function createGalleryItem(arrayImgs) {
     }
 
     parentUl.appendChild(fragment);
-
-    observerIntersection.observe(document.querySelector(['.marker-top']));
-    observerIntersection.observe(document.querySelector(['.marker-bottom']));
-
-    // Оновлюємо SimpleLightbox після додавання зображень
-    if (lightbox) {
-      lightbox.refresh();
-    } else {
-      lightbox = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: 'alt',
-        captionDelay: 250,
-        showCounter: true,
-        disableScroll: true,
-        navText: ['←', '→'],
-        closeText: '×',
-      });
-    }
   }
 }
 
+function intersectionSet() {
+    mainVar.runStarted = true;
+    const observerIntersection = new IntersectionObserver(
+      (entries, observer) => {
+        const situation = {
+          // для контролю положення маркерів div class="intersection-top marker-top" і div class="intersection-bottom marker-bottom"
+          markerTop: false,
+          markerBottom: false,
+        };
+        entries.forEach(entry => {
+          // для кожного контрол. елемента визначаю положення: тут, тобто isIntersecting, чи ні
+          if (entry.isIntersecting) {
+            console.log(entry.target.classList[1]);
+            if (entry.target.classList[1] === 'marker-top') {
+              situation.markerTop = true;
+            } else {
+              if (entry.target.classList[1] === 'marker-bottom') {
+                situation.markerBottom = true;
+              }
+            }
+          }
+        });
+        // визначаю куди проскролили галерею
+        if (situation.markerBottom && !situation.markerTop) {
+          axiosCall(mainVar.searchText, ++page, 18); // додаю записи знизу
+        }
+        console.log(situation);
+        console.log(page);
+      }
+    );
 
+    observerIntersection.observe(document.querySelector(['.marker-top']));
+    observerIntersection.observe(document.querySelector(['.marker-bottom']));
+}
+
+function lightboxRefresh() {
+  // Оновлюємо SimpleLightbox після додавання зображень
+  if (lightbox) {
+    lightbox.refresh();
+  } else {
+    lightbox = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 250,
+      showCounter: true,
+      disableScroll: true,
+      navText: ['←', '→'],
+      closeText: '×',
+    });
+  }
+}
 
 //observerIntersection.observe(document.querySelector('.marker'));
 //observerIntersection.observe(document.querySelector('.marker12'));
